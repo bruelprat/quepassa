d312c3ca318d835186b9e088a85c1794c43cf4cc
bruelprat <bruelprat@gmail.com>
2026-02-23 19:24:52 +0100

    Update script.js

--- script.js
+++ script.js
@@ -114,7 +114,7 @@ function finishVideo() {
 
 // Funció del menú
 function showSection(id) {
-  const sections = ["festa", "regal", "extra","gemma","pagar","vestimenta"];
+  const sections = ["festa", "regal", "extra","gemma","portar"];
   sections.forEach(sec => {
     const el = document.getElementById(sec);
     if (el) el.classList.add("hidden");
@@ -122,4 +122,5 @@ function showSection(id) {
   const t = document.getElementById(id);
   if (t) t.classList.remove("hidden");
 }
+
 
